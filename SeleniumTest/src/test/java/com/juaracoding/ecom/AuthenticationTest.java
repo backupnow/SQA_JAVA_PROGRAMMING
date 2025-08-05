b2fb4c5905cd4d82ce7f71ea9bb9071811080eb7
wilder <gamersmobile30@gmail.com>
2025-08-05 11:28:22 +0700

    done

---
+++ SeleniumTest/src/test/java/com/juaracoding/ecom/AuthenticationTest.java
@@ -0,0 +1,38 @@
+package com.juaracoding.ecom;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.juaracoding.ecom.pages.InventoryPage;
+import com.juaracoding.ecom.pages.LoginPage;
+import com.juaracoding.ecom.providers.DataTestProvider;
+import com.juaracoding.ecom.utils.DriverManager;
+
+@Listeners(ListenerTest.class)
+public class AuthenticationTest {
+
+    @Test(dataProvider = "loginDataProvider", dataProviderClass = DataTestProvider.class)
+    public void loginTest(String username, String password, String expected) throws InterruptedException {
+        DriverManager driverManager = new DriverManager();
+        WebDriver driver = driverManager.getDriver();
+
+        driver.get("https://www.saucedemo.com/v1/index.html");
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.performLogin(username, password);
+        String actual = loginPage.getErrorMessage();
+
+        if (actual == null) {
+            InventoryPage inventoryPage = new InventoryPage(driver);
+            actual = inventoryPage.getCurrentURL();
+        }
+
+        Assert.assertEquals(actual, expected);
+
+        driverManager.quitDriver();
+
+    }
+}
