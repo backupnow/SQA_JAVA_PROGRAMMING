3dde62d2c4aa9b763e3c203c71437f4f21d73a23
wilder <gamersmobile30@gmail.com>
2025-09-05 17:21:42 +0700

    jenkinsTest

--- CuraHealth/src/test/java/com/juaracoding/healthcare/definitions/authentications/Hook.java
+++ CuraHealth/src/test/java/com/juaracoding/healthcare/definitions/authentications/Hook.java
@@ -3,20 +3,17 @@ package com.juaracoding.healthcare.definitions.authentications;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import com.aventstack.extentreports.ExtentReports;
 import com.juaracoding.healthcare.definitions.providers.AuthProviders;
 import com.juaracoding.healthcare.utils.ScenarioContext;
 
 import io.cucumber.java.After;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
 public class Hook {
   
     public static AuthProviders driver;
-    public static ExtentReports extent;
-
+    
   @After
   public void tearDown(Scenario scenario) {
     final byte[] screenshot = ((TakesScreenshot) driver.getDriver()).getScreenshotAs(OutputType.BYTES);
@@ -32,15 +29,5 @@ public class Hook {
   public void beforeScenario(Scenario scenario) {
       ScenarioContext.setScenario(scenario);
   }
-
-  @AfterAll
-  public static void flushReport() {
-      if (extent != null) {
-          extent.createTest("Smoke Test").pass("Pipeline berhasil dijalankan");
-          extent.flush();
-      }
-  }
-
-
     
 }
--- CuraHealth/src/test/java/com/juaracoding/healthcare/definitions/runners/RunnersApp.java
+++ CuraHealth/src/test/java/com/juaracoding/healthcare/definitions/runners/RunnersApp.java
@@ -1,19 +1,18 @@
 package com.juaracoding.healthcare.definitions.runners;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(features = {
-        "src/test/resources/features/AuthCuraHealth.feature",
-        "src/test/resources/features/curahealt.feature",
-        "src/test/resources/features/History.feature",
+        // "src/test/resources/features/AuthCuraHealth.feature",
+        // "src/test/resources/features/curahealt.feature",
+        // "src/test/resources/features/History.feature",
         "src/test/resources/features/Profile.feature"
 }, glue = {
          "com.juaracoding.healthcare.definitions.authentications",
-         "com.juaracoding.healthcare.definitions.orders",
-         "com.juaracoding.healthcare.definitions.scroll",
-         "com.juaracoding.healthcare.definitions.history",
+        //  "com.juaracoding.healthcare.definitions.orders",
+        //  "com.juaracoding.healthcare.definitions.scroll",
+        //  "com.juaracoding.healthcare.definitions.history",
          "com.juaracoding.healthcare.definitions.profile"
 }, plugin = {
         "pretty",  
